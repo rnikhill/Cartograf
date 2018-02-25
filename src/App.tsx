893645c69254d5862d09a4e391e7fb8ff435aa5c
Nikhill Rao <rnikhill@umich.edu>
2018-02-24 20:51:52 -0500

    more boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,25 @@
  * @flow
  */
 
-import * as React from 'react';
+import * as React from "react";
 import {
   Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+} from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  android: "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu",
+  ios: "Press Cmd+R to reload,\n" +
+    "Cmd+D or shake for dev menu",
 });
 
+// tslint:disable-next-line:interface-over-type-literal
 type Props = {};
 export default class App extends React.Component<Props> {
-  render() {
+  public render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -40,19 +41,19 @@ export default class App extends React.Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+  },
+  instructions: {
+    color: "#333333",
+    marginBottom: 5,
+    textAlign: "center",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    textAlign: "center",
   },
 });
